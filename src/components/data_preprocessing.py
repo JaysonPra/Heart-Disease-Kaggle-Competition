c9018e6bde4452af67437596533eb1ea9f8341b3
Jayson Pradhananga <jaysonyt2@gmail.com>
2026-02-06 21:27:13 +0545

    floats_to_convert list loop

--- src/components/data_preprocessing.py
+++ src/components/data_preprocessing.py
@@ -4,7 +4,8 @@ from typing import List
 
 class DataPreprocessor(BaseEstimator, TransformerMixin):
     def __init__(self, categorical_features: List, numerical_features: List):
-        pass
+        self.categorical_features = categorical_features
+        self.numerical_features = numerical_features
 
     def fit(self, X:pd.DataFrame, y:pd.DataFrame):
         return self
@@ -12,13 +13,16 @@ class DataPreprocessor(BaseEstimator, TransformerMixin):
     def transform(self, X:pd.DataFrame):
         X_copy = X.copy()
 
-        X_copy[categorical_features] = X_copy[categorical_features].astype('category')
+        existing_cats = [col for col in self.categorical_features if col in X_copy.columns]
+        if existing_cats:
+            X_copy[existing_cats] = X_copy[existing_cats].astype('category')
 
         floats_to_convert = ['ST depression']
-        ints_to_convert = [col for col in self.numerical_features if col not in floats_to_convert]
+        ints_to_convert = [col for col in self.numerical_features 
+                            if col not in floats_to_convert and col in X_copy.columns]
 
-        if floats_to_convert in X_copy.columns:
-            X_copy[floats_to_convert] = X_copy[floats_to_convert].astype('float32')
+        if all(col in X_copy.columns for col in floats_to_convert):
+            X_copy[floats_to_convert] = X_copy[floats_to_convert].astype('float64')
         X_copy[ints_to_convert] = X_copy[ints_to_convert].astype('int32')
 
         return X_copy
