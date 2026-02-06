6b09f461b815c20d5069d32ade2d2cf969bb220e
Jayson Pradhananga <jaysonyt2@gmail.com>
2026-02-06 21:30:09 +0545

    removed type hinting

--- src/components/feature_engineering.py
+++ src/components/feature_engineering.py
@@ -6,8 +6,8 @@ class FeatureEngineer(BaseEstimator, TransformerMixin):
     def __init__(self):
         pass
 
-    def fit(self, X:pd.DataFrame, y:pd.DataFrame):
+    def fit(self, X, y=None):
         return self
 
-    def transform(self, X:pd.DataFrame):
-        return X
+    def transform(self, X):
+        return X.copy()
