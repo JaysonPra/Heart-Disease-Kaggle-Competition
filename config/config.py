7106b192d6d6d77c400427ed4bf65b1967c511c3
Jayson Pradhananga <jaysonyt2@gmail.com>
2026-02-06 21:25:02 +0545

    corrected experimentation config

--- config/config.py
+++ config/config.py
@@ -3,4 +3,4 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).resolve().parent.parent
 
 RAW_DATA_DIR = ROOT_DIR / "data" / "raw"
-PROCESSED_DATA_DIR = ROOT_DIR / "data" / "processed"
+EXPERIMENTATION_CONFIG_DIR = ROOT_DIR / "config" / "experimentation"
