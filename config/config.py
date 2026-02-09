ad4bf33906de420b6a72d250fcae56fc4df50550
Jayson Pradhananga <jaysonyt2@gmail.com>
2026-02-09 13:07:50 +0545

    directory for processed data

--- config/config.py
+++ config/config.py
@@ -4,3 +4,4 @@ ROOT_DIR = Path(__file__).resolve().parent.parent
 
 RAW_DATA_DIR = ROOT_DIR / "data" / "raw"
 EXPERIMENTATION_CONFIG_DIR = ROOT_DIR / "config" / "experimentation"
+PROCESSED_DATA_DIR = ROOT_DIR / "data" / "processed"
